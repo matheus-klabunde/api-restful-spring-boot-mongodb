3bf9fab988e2c596907c5efa236967bd8ae394d1
Matheus Klabunde <mt.klabunde.dev@gmail.com>
2023-06-06 17:21:16 -0400

    Project created

---
+++ src/main/java/com/matheusklabunde/apirestfulmongo/ApiRestfulSpringBootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.matheusklabunde.apirestfulmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulSpringBootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulSpringBootMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheusklabunde/apirestfulmongo/ApiRestfulSpringBootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusklabunde.apirestfulmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulSpringBootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
