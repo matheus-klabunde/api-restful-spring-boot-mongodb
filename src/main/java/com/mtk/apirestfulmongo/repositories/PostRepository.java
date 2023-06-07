0de5a6a9a20198eb3043c637363a15e265cce01d
Matheus Klabunde <mt.klabunde.dev@gmail.com>
2023-06-07 16:20:28 -0400

    Consulta simples com query methods

--- src/main/java/com/mtk/apirestfulmongo/repositories/PostRepository.java
+++ src/main/java/com/mtk/apirestfulmongo/repositories/PostRepository.java
@@ -1,9 +1,14 @@
 package com.mtk.apirestfulmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.mtk.apirestfulmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	// REFERÊNCIA: https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/ -> (Buscar por Query)
+	//             https://docs.spring.io/spring-data/data-document/docs/current/reference/html/
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
