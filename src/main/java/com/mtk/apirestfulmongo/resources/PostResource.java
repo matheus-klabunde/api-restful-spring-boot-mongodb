173ce69c1b04e8c691cf2c512b9cc35cbd360e45
Matheus Klabunde <mt.klabunde.dev@gmail.com>
2023-06-07 15:15:03 -0400

    Obtendo um post por id

---
+++ src/main/java/com/mtk/apirestfulmongo/resources/PostResource.java
@@ -0,0 +1,26 @@
+package com.mtk.apirestfulmongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mtk.apirestfulmongo.domain.Post;
+import com.mtk.apirestfulmongo.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Post> findByid(@PathVariable String id) {
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
