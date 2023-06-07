5eb9e4a2f74b5c4592a0714fbc037395fd41d69d
Matheus Klabunde <mt.klabunde.dev@gmail.com>
2023-06-07 17:16:33 -0400

    Consulta com vários critérios

--- src/main/java/com/mtk/apirestfulmongo/repositories/PostRepository.java
+++ src/main/java/com/mtk/apirestfulmongo/repositories/PostRepository.java
@@ -1,5 +1,6 @@
 package com.mtk.apirestfulmongo.repositories;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -10,11 +11,13 @@ import com.mtk.apirestfulmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 
 	// REFERÊNCIA: https://www.mongodb.com/docs/manual/reference/operator/query/regex/
-	// "?0" -> serve para aproveitar o PRIMEIRO parâmetro obtido pelo método. (?x)
 	@Query("{'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
 	
 	// REFERÊNCIA: https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/ -> (Buscar por Query)
 	//             https://docs.spring.io/spring-data/data-document/docs/current/reference/html/
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ { date: { $gte: ?1 } }, { date: { $lte: ?2 } } , { $or: [ {'title': { $regex: ?0, $options: 'i' } }, {'body': { $regex: ?0, $options: 'i' } }, {'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
