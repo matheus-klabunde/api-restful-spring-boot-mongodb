e939b1995961c2c385e661c896d13dc46e00a94c
Matheus Klabunde <mt.klabunde.dev@gmail.com>
2023-06-07 16:34:59 -0400

    Consulta simples com @Query

--- src/main/java/com/mtk/apirestfulmongo/repositories/PostRepository.java
+++ src/main/java/com/mtk/apirestfulmongo/repositories/PostRepository.java
@@ -3,11 +3,17 @@ package com.mtk.apirestfulmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.mtk.apirestfulmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	// REFERÊNCIA: https://www.mongodb.com/docs/manual/reference/operator/query/regex/
+	// "?0" -> serve para aproveitar o PRIMEIRO parâmetro obtido pelo método. (?x)
+	@Query("{'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	// REFERÊNCIA: https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/ -> (Buscar por Query)
 	//             https://docs.spring.io/spring-data/data-document/docs/current/reference/html/
 	List<Post> findByTitleContainingIgnoreCase(String text);
